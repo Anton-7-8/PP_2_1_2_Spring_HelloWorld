e1de43a3cca6a2e8b3f1f9035ae73f1f569351ea
Anton-7-8 <Anton-7-8@yandex.ru>
2024-05-04 17:46:06 +0300

    Задача выполнена.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,28 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getValue());
+
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getValue());
+
+        boolean comparison1 = bean1 == bean2;
+        boolean comparison2 = bean3 == bean4;
+
+        System.out.println(comparison1);
+        System.out.println(comparison2);
+
+
     }
 }
